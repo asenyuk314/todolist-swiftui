280c5c77ac96308af0196299a61de857d995460e
Alexander Senyuk <a.senyuk314@gmail.com>
2021-12-29 18:03:11 +0300

    Initial Commit

---
+++ TodoList-SwiftUI/TodoList_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoList_SwiftUIApp.swift
+//  TodoList-SwiftUI
+//
+//  Created by Александр Сенюк on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
